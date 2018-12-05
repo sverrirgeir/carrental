5c8d0a782f5298980b1dc34515e48852b2cab2b3
kristofere17 <kristofer_ey@hotmail.com>
2018-12-05 15:52:35 +0000

    uppfært carmenu

--- UI/CarsUI.py
+++ UI/CarsUI.py
@@ -1,19 +1,19 @@
-from services.carservice import carservice
+from services.CarService import CarService
 
 class CarsUI:
     def __init__(self):
         self.__cars = CarService()
 
-def print_car_menu():
-    """Prints out the cars menu and returns a input sentence asking for a choice"""
-    choice1 = "1. Listi yfir Bílaflota"
-    choice2 = "2. Bílar í Útleigu"
-    choice3 = "3. Fletta upp Bíl"
-    choice4 = "4. Lausir Bílar"
-    choice5 = "5. Til baka"
-    print("")
-    print("\t{:^10}".format("Bílaleiga ehf"))
-    print("")
-    print("\t{:<30}\n\t{:<10}\n\t{:<10}\n\t{:<10}\n\t{:<10}".format(choice1, choice2, choice3, choice4, choice5,))
-    print("")
-    return input("\tValmöguleiki: ")
+    def print_car_menu(self):
+        """Prints out the cars menu and returns a input sentence asking for a choice"""
+        choice1 = "1. Listi yfir Bílaflota"
+        choice2 = "2. Bílar í Útleigu"
+        choice3 = "3. Fletta upp Bíl"
+        choice4 = "4. Lausir Bílar"
+        choice5 = "5. Til baka"
+        print("")
+        print("\t{:^10}".format("Bílaleiga ehf"))
+        print("")
+        print("\t{:<30}\n\t{:<10}\n\t{:<10}\n\t{:<10}\n\t{:<10}".format(choice1, choice2, choice3, choice4, choice5,))
+        print("")
+        return input("\tValmöguleiki: ")
