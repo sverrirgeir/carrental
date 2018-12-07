c0c59065365f5f4ab5de69c61c358f8983143649
kristofere17 <kristofer_ey@hotmail.com>
2018-12-07 11:55:51 +0000

    fletta upp viðskiptavin lagað

--- repositories/CustomerRepo.py
+++ repositories/CustomerRepo.py
@@ -6,12 +6,21 @@ class CustomerRepo:
     
     def find_customer(self, passport):
         with open("./data/customer.txt", "r") as customerfile:
+            list_of_lists = []
+            not_found = 0
             for line in customerfile:
-                line_list = line.split(",")
-                if line_list[1] == passport:
-                    name = line_list[0]
-                    passport = line_list[1]
-                    kredit = line_list[2]
+                line_list = line.strip().split(",")
+                list_of_lists.append(line_list)
+            for i in range(len(list_of_lists)):
+                if list_of_lists[i][1] == passport:
+                    not_found = 1
+                    name = list_of_lists[i][0]
+                    passport = list_of_lists[i][1]
+                    kredit = list_of_lists[i][2]
                     return name, passport, kredit
+                else:
+                    not_found = 0
+            if not_found == 0:
+                return "Not found", "Not found", "Not found"
 
                                              
