75230354fb5579ca058945e01942604357b2a759
Gretarkri <gretarkri@gmail.com>
2018-12-07 12:31:53 +0000

    Útfærði lista yfir bílaflota í 3ja laga hönnun

--- Models/Car.py
+++ Models/Car.py
@@ -1,1 +1,15 @@
+class Car:
+
+    def __init__(self, model, year, plate, miles, color, fuel_type, catagory, status):
+        self.model = model
+        self.year = year
+        self.plate = plate
+        self.miles = miles
+        self.color = color
+        self.fuel_type = fuel_type
+        self.catagory = catagory
+        self.status = status
+
+    def __str__(self):
+        return "{} {} {} {} {} {} {} {}".format(self.model, self.year, self.plate, self.miles, self.color, self.fuel_type, self.catagory, self.status)
 
