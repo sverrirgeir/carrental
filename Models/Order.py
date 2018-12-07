213f79ee1e4c32fc7d6c3d65e40eb325f0e23ca0
Halldor Arni <halldorg17@ru.is>
2018-12-07 13:17:00 +0000

    Order klasi gerður

--- Models/Order.py
+++ Models/Order.py
@@ -1,1 +1,23 @@
+class Order:
+    def __init__(self, passport, day1, day2, amount):
+        self.passport = passport 
+        self.pick_up_day = day1
+        self.return_day = day2
+        self.amount = amount
+
+    def __str__ (self):
+        return "{} {} {} {}".format( self.passport, self.pick_up_day, self.return_day, self.amount)
+
+    def get_passport(self):
+        return self.passport
+    
+    def get_pickup_day(self):
+        return self.pick_up_day
+    
+    def get_return_day(self):
+        return self.return_day
+    
+    def get_amount(self):
+        return self.amount
+    
 
