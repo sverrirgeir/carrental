c8b17b01c132ff3f05a245534474a26ab6a47ea7
sverrirgeir <sverrirgeir@gmail.com>
2018-12-11 12:16:04 +0000

    :)

--- repositories/CarRepo.py
+++ repositories/CarRepo.py
@@ -145,6 +145,9 @@ class CarRepo:
             if not_found == 0:
                 return 0,0,0,0,0,0,0,0       
 
+
+
+    ''' það vantar að klára að útfæra '''
     def delete_cars(self, car_plate):
         with open("./data/cars.txt", "r") as car_string:
             list_of_lists = []
@@ -152,18 +155,19 @@ class CarRepo:
                 line_list = line.strip().split(",")
                 list_of_lists.append(line_list)
             for i in range(len(list_of_lists)):
-                if list_of_lists[2] == car_plate:
+                test = list_of_lists[i][2]
+                if test == car_plate:
                     list_of_lists.remove(list_of_lists[i])
-            with open("./data/cars.txt", "w") as car_string:
-                for lst in list_of_lists:
-                    car_model= lst[0]
-                    car_year = lst[1]
-                    car_plate = lst[2]
-                    car_miles = lst[3]
-                    car_color = lst[4]
-                    car_fuel_type = lst[5]
-                    car_status = lst[6]
-                    car_catagory = lst[7]
-                    car_string.write("{},{},{},{},{},{},{},{}".format(car_model, car_year, car_plate, car_miles, car_color, car_fuel_type, car_status, car_catagory))
-                    car_string.write("\n")
-                return
+        with open("./data/cars.txt", "w") as car_string:
+            for lst in list_of_lists:
+                car_model= lst[0]
+                car_year = lst[1]
+                car_plate = lst[2]
+                car_miles = lst[3]
+                car_color = lst[4]
+                car_fuel_type = lst[5]
+                car_status = lst[6]
+                car_catagory = lst[7]
+                car_string.write("{},{},{},{},{},{},{},{}".format(car_model, car_year, car_plate, car_miles, car_color, car_fuel_type, car_status, car_catagory))
+                car_string.write("\n")
+        return
