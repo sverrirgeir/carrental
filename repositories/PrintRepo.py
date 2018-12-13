6166e286dc4f100e6d102ba100c643a89ab5d18e
zakkimoto <zakkitv@gmail.com>
2018-12-13 11:09:03 +0000

    kommenta

--- repositories/PrintRepo.py
+++ repositories/PrintRepo.py
@@ -2,13 +2,14 @@ import smtplib
 
 class PrintRepo():    
     def send_email(self, name, inday, outday):
-            our_email = 'bilaleigaehf@gmail.com'
-            password = 'Verklegt1'
-            send_to_email = input("\n\tEmail: ")
-            message = name + "\n" + " \nPontun Stadfest " + "\nFra: " + str(inday) + " Til: " + str(outday)
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
-            server.login(our_email, password)
-            server.sendmail(our_email, send_to_email , message)
-            server.quit()
-            return
+        '''Sendum email með "pöntun staðfest" á aðilann sem var að panta frá okkur með nafni og dagsetningu'''
+        our_email = 'bilaleigaehf@gmail.com'
+        password = 'Verklegt1'
+        send_to_email = input("\n\tEmail: ")
+        message = name + "\n" + " \nPontun Stadfest " + "\nFra: " + str(inday) + " Til: " + str(outday)
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(our_email, password)
+        server.sendmail(our_email, send_to_email , message)
+        server.quit()
+        return
